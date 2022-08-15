c44db2c09265970496ef2ca83b53f259b4f5eedf
TestFlip <james@Testflip.co.uk>
2022-08-15 17:23:37 +0100

    Add libraries and update execution script

--- Framework/buildRun.bat
+++ Framework/buildRun.bat
@@ -2,11 +2,11 @@ REM you'll have to find the "latest" version of where msbuild.exe resides on you
 REM set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v2.0.50727
 REM set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v3.5
 REM set msBuildDir=C:\Program Files (x86)\MSBuild\12.0\Bin
-set msBuildDir=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin
+set msBuildDir=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin
 
-nuget restore TestModellerCSharp.sln
+"libs\nuget\nuget.exe" restore TestModellerCSharp.sln
 
 call "%msBuildDir%\msbuild.exe"  TestModellerCSharp.sln /p:Configuration=Debug  /l:FileLogger,Microsoft.Build.Engine;logfile=Manual_MSBuild_ReleaseVersion_LOG.log
 set msBuildDir=
 
-nunit3-console.exe bin\Debug\TestModellerCSharp.exe --test="Tests.%1"
+"libs\NUnit.org\nunit-console\nunit3-console.exe" "bin\Debug\TestModellerCSharp.exe" --test="Tests.%1"
